65356e3e1d37463d2c77d80d381a1a1fb0e61493
djaysllan <76598129+djaysllan@users.noreply.github.com>
2021-01-02 15:32:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -448,14 +448,14 @@ async function starts() {
 						fs.unlinkSync(rano)
 					})
 					break
-				case 'tagall':
+				case 'Convocar':
 					if (!isGroup) return reply(mess.only.group)
 					if (!isGroupAdmins) return reply(mess.only.admin)
 					members_id = []
 					teks = (args.length > 1) ? body.slice(8).trim() : ''
 					teks += '\n\n'
 					for (let mem of groupMembers) {
-						teks += `*#* @${mem.jid.split('@')[0]}\n`
+						teks += `oi *#* @${mem.jid.split('@')[0]}\n`
 						members_id.push(mem.jid)
 					}
 					mentions(teks, members_id, true)
